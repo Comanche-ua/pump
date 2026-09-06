371323dc6f132782179b999ad28ebe31e9ef54a2
Comanche-ua <alexandr.dn@gmail.com>
2026-09-06 22:22:03 +0300

    Delete src directory

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Dashboard } from "@/components/dashboard";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <Dashboard />;
-}
